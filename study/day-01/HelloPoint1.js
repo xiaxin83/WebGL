1d0a2d2e2470c082273e6850fa67bd33e476c042
lebron.xia <1031505541@qq.com>
2023-03-11 17:24:21 +0800

    2023/3/11 17：24 学习webGL

--- study/day-01/HelloPoint1.js
+++ study/day-01/HelloPoint1.js
@@ -3,7 +3,7 @@
  */
 const VSHADER_SOURCE =
   "void main() {\n" +
-  "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n" + // Set the vertex coordinates of the point
+  "  gl_Position = vec4(0.5, 0.5, 0.0, 1.0);\n" + // Set the vertex coordinates of the point
   "  gl_PointSize = 10.0;\n" + // Set the point size
   "}\n";
   /**
@@ -31,5 +31,6 @@ const FSHADER_SOURCE =
 
   gl.clear(gl.COLOR_BUFFER_BIT);
   // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 1);
+    
 })();
